030e343cf23cb35cce99bc50f92f2953d5557efb
Ida <idajosefinolsson@gmail.com>
2023-05-11 21:57:54 +0200

    result screen + organizing
    finally made it work so that the result screen is visible when you lose + what score you get

--- maingame.js
+++ maingame.js
@@ -10,19 +10,8 @@ let upTime;
 let downTime;
 let displayScore = document.getElementById('score');
 let score = 0;
-
-// let modifier = 20;
-// // moving left and right
-// window.addEventListener('keydown', (event) => {
-//   switch (event.key) {
-//     case 'ArrowRight':
-//       character.style.left = (parseInt(getComputedStyle(character).left) + modifier) + "px";
-//       break;
-//     case 'ArrowLeft':
-//       character.style.left = (parseInt(getComputedStyle(character).left) - modifier) + "px";
-//       break;
-//   }
-// });
+let finalScore = 0;
+let activeGame = true;
 
 //jumping function
 function jump() {
@@ -40,14 +29,14 @@ function jump() {
       characterBottom -= 10;
       character.style.bottom = characterBottom + 'px';
     //the number below steers how fast the character lands currently at 20
-    }, 20);
+    }, 15);
   }
   //how high the character jumps is the number 10 below
-    characterBottom += 15;
+    characterBottom += 10;
     character.style.bottom = characterBottom + 'px';
     isJumping = true;
 //the number below steers how fast the character lands currently at 20
-  }, 20);
+  }, 15);
 }
 
 //this shows the high score in the top left corner
@@ -69,7 +58,6 @@ function generateObstacle(){
   let obstacleBottom = 70;
   let obstacleWidth = 50;
   let obstacleHeight = 50;
-  // Math.floor(Math.random() * 40) + 20; this one we will use instead how often the obstacles show up
 
   function moveObstacle(){
   //the number below 15 lets us know how fast or slow it goes in a way...
@@ -78,16 +66,20 @@ function generateObstacle(){
     obstacle.style.bottom = obstacleBottom + 'px';
     obstacle.style.width = obstacleWidth + 'px';
     obstacle.style.height = obstacleHeight + 'px';
-    if(characterRight >= obstacleRight - characterWidth && characterRight <= 
-      obstacleRight + obstacleWidth && characterBottom <= obstacleBottom + obstacleHeight) {
-      alert('Game Over! Your score is: '+score);
-      clearInterval(obstacleInterval);
-      clearTimeout(obstacleTimeout);
-      location.reload();
+    if(activeGame == true && characterRight >= obstacleRight - characterWidth && characterRight <= 
+      obstacleRight && characterBottom <= obstacleBottom + obstacleHeight) {
+      // alert('Game Over! Your score is: '+score);
+      activeGame = false;
+      document.getElementById("end").style.visibility = "visible";
+      //fix so that the counter stops
+      finalScore = score;
+      document.getElementById("endScore").innerHTML = finalScore;
+      // clearInterval(obstacleInterval);
+      // clearTimeout(obstacleTimeout);
     }
   }
   let obstacleInterval = setInterval(moveObstacle, 50);
-  let obstacleTimeout = setTimeout(generateObstacle, 1000);
+  let obstacleTimeout = setTimeout(generateObstacle, Math.floor(Math.random() * 1000) + 500);
 }
 
 generateObstacle();
@@ -99,13 +91,37 @@ function control(e) {
   }
 }
 
-//the keydown allows the character to jump when both the up arrow and space key is down apparently?
+//the keydown allows the character to jump when the up arrow is down.
 document.addEventListener('keydown', control);
 
-//how to use math.random to have the obstacles appear with random distance between
-//how to make the obstacles appear as different characters between four or five png/jpg
-//if we want to keep the left and right movement then what must be done for it not to call game over if you move?
-//the character hits the obstacle but it stops a bit before? how do we change that?
-//how do we make a five life with the hearts that removes them, what code can we use?
+function reload() {
+  location.reload();
+}
 
 //add background image here to make it stop when the rabbit dies.
+//change source attribute for rabbit, sprite.
+//max lives and min lives loop life number, paint pictures next to eachother
+//request animation frame (for intervals)-put all interval function into one (draw)
+
+
+
+//how to use parallax effect for a number of images to move with different speeds?
+//how do we make the animation of the background stop when the character dies?
+//how do we add lives: 5 at the top right corner and how do you code so when you die you lose a life?
+//how do we create an interval function to put all interval into one function?
+//how do we make the rabbit into a sprite? we have him normal, jumping with paws out and one where he's dead
+//the counter won't stop on the result screen
+
+//if we want our rabbit to move left and right
+// let modifier = 20;
+// // moving left and right
+// window.addEventListener('keydown', (event) => {
+//   switch (event.key) {
+//     case 'ArrowRight':
+//       character.style.left = (parseInt(getComputedStyle(character).left) + modifier) + "px";
+//       break;
+//     case 'ArrowLeft':
+//       character.style.left = (parseInt(getComputedStyle(character).left) - modifier) + "px";
+//       break;
+//   }
+// });
