f1a63605e10b06c3cbe2f122d69b666fc4b148d3
Nelia <cornelia.json@hotmail.com>
2023-05-25 23:13:23 +0200

    clean up

--- maingame.js
+++ maingame.js
@@ -25,9 +25,8 @@ let activeGame = true;
 let gameOver = false; // Variable to track game state
 let obstacles = []; // Array to store the obstacles
 
-//function to generate the snail
+//function to generate the obstacles
 function generateObstacle() {
-  //här kan du lägga till mer karaktärer och ändra dem. namn på dem i images folder är viktig
   const obstacleArray = ["snail", "ladybug", "grasshopper", "bee", "fly"];
   let randomObstacle =
     obstacleArray[Math.floor(Math.random() * obstacleArray.length)];
@@ -48,9 +47,7 @@ function generateObstacle() {
   obstacle.style.bottom = obstacleBottom + "px";
   obstacle.style.width = obstacleWidth + "px";
   obstacle.style.height = obstacleHeight + "px";
-  //Dessa kontrollerar bordern runt insekterna
   obstacle.style.border = "solid 0px black";
-  // obstacle.style.borderRadius = "50%";
   obstacle.style.backgroundPosition = "top";
 
   function moveObstacle() {
@@ -107,7 +104,7 @@ function winGame() {
 function jump() {
   if (isJumping) return;
   upTime = setInterval(() => {
-    //how high up the character jumps is the number currently at 350
+    //how high up the character jumps is the number currently at 300
     if (characterBottom >= groundHeight + 300) {
       clearInterval(upTime);
       downTime = setInterval(() => {
@@ -121,7 +118,7 @@ function jump() {
         //the number below steers to slow down the speed of the landing of the character.
       }, 21);
     }
-    //how fast the character jumps is the number 5 below
+    //how fast the character jumps is the number 4 below
     characterBottom += 4;
     character.style.bottom = characterBottom + "px";
     isJumping = true;
@@ -132,10 +129,9 @@ function jump() {
 // function to show the score
 function showScore() {
   if (!activeGame) return;
-  // if (gameOver) return;
   score++;
   displayScore.innerText = score;
-  //change score from 100 to 600 before sending in the game!!!!
+  // if you score 600 you win the game
   if (score >= 600) {
     // call the function that handles winning the game
     winGame();
@@ -154,166 +150,3 @@ function control(e) {
 function reload() {
   location.reload();
 }
-
-// let obstacleSources = [
-//   "images/snail.png",
-//   "images/ladybug.png",
-//   "images/fly.png",
-// ];
-
-// // Select a random obstacle source
-// let randomSource = obstacleSources[Math.floor(Math.random() * obstacleSources.length)];
-
-// let obstacle = document.createElement("div");
-// obstacle.setAttribute("class", "obstacle");
-// obstacle.style.backgroundImage = `url(${randomSource})`;
-// obstacles.push(obstacle);
-// document.querySelector(".obstacles").appendChild(obstacle);
-
-// let obstacleRight = -270;
-// let obstacleBottom = -30;
-// // let obstacleWidth = 200;
-// // let obstacleHeight = 200;
-// var obstacleWidth = randomObstacle.width;
-// var obstacleHeight = randomObstacle.height;
-
-// function moveObstacle() {
-//   obstacleRight += 10;
-//   obstacle.style.right = obstacleRight + "px";
-//   obstacle.style.bottom = obstacleBottom + "px";
-//   obstacle.style.width = obstacleWidth + "px";
-//   obstacle.style.height = obstacleHeight + "px";
-
-//   // Collision detection
-//   if (
-//     activeGame &&
-//     characterRight >= obstacleRight - characterWidth &&
-//     characterRight <= obstacleRight &&
-//     characterBottom <= obstacleBottom + obstacleHeight
-//   ) {
-//     // Collision occurred, call loseGame function
-//     loseGame();
-//   }
-// }
-
-// let obstacleInterval = setInterval(moveObstacle, 50);
-// let obstacleTimeout = setTimeout(
-//   generateObstacle,
-//   Math.floor(Math.random() * 4000) + 3000
-// );
-
-// Old code
-// let character = document.getElementById('character')
-// let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'));
-// let characterRight = parseInt(window.getComputedStyle(character).getPropertyValue('right'));
-// let characterWidth = parseInt(window.getComputedStyle(character).getPropertyValue('width'));
-// let ground = document.getElementById('ground');
-// let groundBottom = parseInt(window.getComputedStyle(ground).getPropertyValue('bottom'));
-// let groundHeight = parseInt(window.getComputedStyle(ground).getPropertyValue('height'));
-// let isJumping = false;
-// let upTime;
-// let downTime;
-// let displayScore = document.getElementById('score');
-// let score = 0;
-// let finalScore = 0;
-// let activeGame = true;
-
-// //jumping function
-// function jump() {
-//   if(isJumping) return;
-//   upTime = setInterval(() => {
-// //how high up the character jumps is the number currently at 150
-//     if (characterBottom >= groundHeight + 180) {
-//       clearInterval(upTime);
-//       downTime = setInterval(() => {
-//         //the number 10 below makes sure the character lands on the grass
-//         if(characterBottom <= groundHeight + 10) {
-//           clearInterval(downTime);
-//           isJumping = false;
-//         }
-//       characterBottom -= 10;
-//       character.style.bottom = characterBottom + 'px';
-//     //the number below steers how fast the character lands currently at 20
-//     }, 15);
-//   }
-//   //how high the character jumps is the number 10 below
-//     characterBottom += 10;
-//     character.style.bottom = characterBottom + 'px';
-//     isJumping = true;
-// //the number below steers how fast the character lands currently at 20
-//   }, 15);
-// }
-
-// //this shows the high score in the top left corner
-// function showScore() {
-//   score++;
-//   displayScore.innerText = score;
-// }
-
-// setInterval(showScore, 100);
-
-// //this is the random way to display the obstacles.
-// function generateObstacle(){
-//   let obstacles = document.querySelector('.obstacles');
-//   let obstacle = document.createElement('div');
-//   obstacle.setAttribute('class', 'obstacle');
-//   obstacles.appendChild(obstacle);
-
-//   let obstacleRight = -20;
-//   let obstacleBottom = 70;
-//   let obstacleWidth = 50;
-//   let obstacleHeight = 50;
-
-//   function moveObstacle(){
-//     obstacleRight += 15;
-//     obstacle.style.right = obstacleRight + 'px';
-//     obstacle.style.bottom = obstacleBottom + 'px';
-//     obstacle.style.width = obstacleWidth + 'px';
-//     obstacle.style.height = obstacleHeight + 'px';
-//     if(activeGame == true && characterRight >= obstacleRight - characterWidth && characterRight <=
-//       obstacleRight && characterBottom <= obstacleBottom + obstacleHeight) {
-//       activeGame = false;
-//       document.getElementById("end").style.visibility = "visible";
-//       finalScore = score;
-//       document.getElementById("endScore").innerHTML = finalScore;
-//     }
-//   }
-//   let obstacleInterval = setInterval(moveObstacle, 50);
-//   let obstacleTimeout = setTimeout(generateObstacle, Math.floor(Math.random() * 1000) + 400);
-// }
-
-// generateObstacle();
-
-// //this is the control function of the arrowup key to make the character jump
-// function control(e) {
-//   if (e.key == 'ArrowUp') {
-//     jump();
-//   }
-// }
-
-// //the keydown allows the character to jump when the up arrow is down.
-// document.addEventListener('keydown', control);
-
-// function reload() {
-//   location.reload();
-// }
-
-//add background image here to make it stop when the rabbit dies.
-//max lives and min lives loop life number, paint pictures next to eachother
-//request animation frame (for intervals)-put all interval function into one (draw)
-//how do we add lives: 5 at the top right corner and how do you code so when you die you lose a life?
-//how do we make the rabbit into a sprite? we have him normal, jumping with paws out and one where he's dead
-
-//if we want our rabbit to move left and right
-// let modifier = 20;
-// // moving left and right
-// window.addEventListener('keydown', (event) => {
-//   switch (event.key) {
-//     case 'ArrowRight':
-//       character.style.left = (parseInt(getComputedStyle(character).left) + modifier) + "px";
-//       break;
-//     case 'ArrowLeft':
-//       character.style.left = (parseInt(getComputedStyle(character).left) - modifier) + "px";
-//       break;
-//   }
-// });
