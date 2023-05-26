d49d7abfa0584bb6477470649c21c88bfce53e55
Nelia <cornelia.json@hotmail.com>
2023-05-26 11:39:47 +0200

    last clean up and changes

--- maingame.js
+++ maingame.js
@@ -1,3 +1,4 @@
+//The following 20 lines of code were adapted from https://www.youtube.com/watch?v=a0TyCnFgqlk&t=263s
 let character = document.getElementById("character");
 let characterBottom = parseInt(
   window.getComputedStyle(character).getPropertyValue("bottom")
@@ -18,22 +19,28 @@ let groundHeight = parseInt(
 let isJumping = false;
 let upTime;
 let downTime;
+// The following 3 lines of code were adapted from https://www.youtube.com/watch?v=IO1M_SO0ow4
 let displayScore = document.getElementById("score");
 let score = 0;
 let finalScore = 0;
+// The following lines of code was added by courtesy of Chat GPT.
 let activeGame = true;
 let gameOver = false; // Variable to track game state
 let obstacles = []; // Array to store the obstacles
 
 //function to generate the obstacles
+//The following 43 lines of code were adapted from https://www.youtube.com/watch?v=a0TyCnFgqlk&t=263s
 function generateObstacle() {
-  const obstacleArray = ["snail", "ladybug", "grasshopper", "fly"];
+  // The following 3 lines of code were added by courtesy of Emma Åkerberg.
+  const obstacleArray = ["snail", "ladybug", "grasshopper", "fly", "bug"];
   let randomObstacle =
     obstacleArray[Math.floor(Math.random() * obstacleArray.length)];
   let obstacle = document.createElement("div");
   obstacle.setAttribute("class", "obstacle");
 
+  // The following 1 line of code was added by courtesy of Emma Åkerberg.
   obstacle.style.backgroundImage = "url('images/" + randomObstacle + ".png";
+  //The following 1 line of code was added by courtesy of Chat GPT.
   obstacles.push(obstacle);
   document.querySelector(".obstacles").appendChild(obstacle);
 
@@ -47,8 +54,7 @@ function generateObstacle() {
   obstacle.style.bottom = obstacleBottom + "px";
   obstacle.style.width = obstacleWidth + "px";
   obstacle.style.height = obstacleHeight + "px";
-  obstacle.style.backgroundPosition = "center";
-  obstacle.style.border = "solid 1px black";
+  obstacle.style.backgroundPosition = "top";
 
   function moveObstacle() {
     obstacleRight += 10;
@@ -58,8 +64,8 @@ function generateObstacle() {
     if (
       activeGame &&
       characterRight >= obstacleRight - characterWidth &&
-      characterRight <= obstacleRight + 120 &&
-      characterBottom <= obstacleBottom + obstacleHeight - 40
+      characterRight <= obstacleRight + -35 &&
+      characterBottom <= obstacleBottom + obstacleHeight - 30
     ) {
       // collision occurred, call loseGame function
       loseGame();
@@ -71,6 +77,7 @@ function generateObstacle() {
 }
 
 // main function that manages intervals and event listeners
+// The following 28 lines of code were added by courtesy of Chat GPT.
 function startGame() {
   gameOver = false;
   activeGame = true;
@@ -90,6 +97,7 @@ function startGame() {
 function loseGame() {
   gameOver = true;
   activeGame = false;
+  // The following 1 line of code was adapted from https://www.youtube.com/watch?v=fYoFzkPC2O4&list=WL&index=24
   document.getElementById("end").style.visibility = "visible";
   finalScore = score;
   document.getElementById("endScore").innerHTML = finalScore;
@@ -98,9 +106,12 @@ function loseGame() {
 function winGame() {
   gameOver = true;
   activeGame = false;
+  // The following 1 line of code was adapted from https://www.youtube.com/watch?v=fYoFzkPC2O4&list=WL&index=24
   document.getElementById("win").style.visibility = "visible";
 }
 
+// character jump function
+//The following 24 lines of code were adapted from https://www.youtube.com/watch?v=a0TyCnFgqlk&t=263s
 function jump() {
   if (isJumping) return;
   upTime = setInterval(() => {
@@ -122,16 +133,18 @@ function jump() {
     characterBottom += 4;
     character.style.bottom = characterBottom + "px";
     isJumping = true;
-    //the number below steers how fast the character lands currently at 15 ------------ same thing here. what's the difference?
+    //the number below steers how fast the character flies up
   }, 21);
 }
 
 // function to show the score
+// The following 4 lines of code were adapted from https://www.youtube.com/watch?v=IO1M_SO0ow4
 function showScore() {
   if (!activeGame) return;
   score++;
   displayScore.innerText = score;
   // if you score 600 you win the game
+  // The following 5 lines of code were added by courtesy of Chat GPT.
   if (score >= 600) {
     // call the function that handles winning the game
     winGame();
@@ -139,6 +152,7 @@ function showScore() {
 }
 
 // function to handle the ArrowUp key for jumping
+//The following 5 lines of code were adapted from https://www.youtube.com/watch?v=a0TyCnFgqlk&t=263s
 function control(e) {
   if (e.key === "ArrowUp") {
     jump();
@@ -146,6 +160,7 @@ function control(e) {
 }
 
 // function to reload the page
+// The following 2 lines of code were adapted from https://www.youtube.com/watch?v=fYoFzkPC2O4&list=WL&index=24
 function reload() {
   location.reload();
 }
